b585d3a79b1e17ef2977fc002d69443ce5f4bb2c
unknown <ni261rot@fhkn.de>
2025-05-16 09:28:03 +0200

    Fix: Umbenennung und letzte Änderungen an tui.scala

--- src/main/scala/de/htwg/codebreaker/Main.scala
+++ src/main/scala/de/htwg/codebreaker/Main.scala
@@ -3,10 +3,10 @@ package de.htwg.codebreaker
 import de.htwg.codebreaker.model.game._
 
 import de.htwg.codebreaker.controller.Controller
-import de.htwg.codebreaker.view.TUI
+import de.htwg.codebreaker.view.tui
 
 object Codebreaker:
   def main(args: Array[String]): Unit =
     val (gameModel, gameState) = GameFactory.createDefaultGame()
     val controller = new Controller(gameModel, gameState)
-    val tui = new TUI(controller)
+    val tui = new tui(controller)
--- src/main/scala/de/htwg/codebreaker/view/tui.scala
+++ src/main/scala/de/htwg/codebreaker/view/tui.scala
@@ -1,50 +1,64 @@
-// package de.htwg.codebreaker.view
-// import de.htwg.codebreaker.model._
-//   import de.htwg.codebreaker.model.MapObject._
+package de.htwg.codebreaker.view
 
-// object TUI:
+import de.htwg.codebreaker.controller.Controller
+import de.htwg.codebreaker.util.Observer
+import de.htwg.codebreaker.model._
+import de.htwg.codebreaker.model.MapObject._
+
+class tui(controller: Controller) extends Observer:
+  controller.add(this)
+
+  def processInputLine(input: String): Unit =
+  input match
+    case "q" =>
+      println("Spiel beendet.")
+    case "m" =>
+      show()
+    case _ =>
+      println("Unbekannter Befehl.")
 
 
+  def show(): Unit =
+    val players = controller.getPlayers
+    val servers = controller.getServers
+    val map = controller.getMapData()
 
-//   def displayMap(map: WorldMap, players: List[Player], servers: List[Server]): String =
-//     val mapData = map.getMapData(players, servers)
+    println("Willkommen zu Codebreaker!")
+    println("== Codebreaker: Weltkarte ==")
+    println(displayMap(map))
+    println()
+    players.zipWithIndex.foreach { case (p, i) =>
+      println(s"Spieler $i: ${p.name} @ (${p.tile.x}, ${p.tile.y}) [${p.tile.continent.short}]")
+      println(s"CPU: ${p.cpu} | RAM: ${p.ram} | Code: ${p.code}")
+      println(s"Level: ${p.level} | XP: ${p.xp} | Security: ${p.cybersecurity}%")
+      println("-" * 30)
+    }
+    println("== Serverliste ==")
+    printServerList(servers)
 
-//     mapData.map { row =>
-//       row.map {
-//         case PlayerAndServerTile(p, s, _, cont) => f"[P$p/S$s]"
-//         case PlayerOnTile(i)                          => f"[P$i]"
-//         case ServerOnTile(i, _, cont) => f"[$i]S-${cont.short}"
-//         case EmptyTile(_)                             => " .   "
-//       }.mkString(" ")
-//     }.mkString("\n")
+  def displayMap(mapData: Vector[Vector[MapObject]]): String =
+    mapData.map { row =>
+      row.map {
+        case PlayerAndServerTile(p, s, _, _) => f"[P$p/S$s]"
+        case PlayerOnTile(i)                 => f"[P$i]"
+        case ServerOnTile(i, _, cont)        => f"[$i]S-${cont.short}"
+        case EmptyTile(_)                    => " .   "
+      }.mkString(" ")
+    }.mkString("\n")
 
+  def printServerList(servers: List[Server]): Unit =
+    servers.zipWithIndex.foreach { case (server, index) =>
+      val tile = server.tile
+      val contShort = tile.continent.short
+      val pos = (tile.x, tile.y)
+      val rewardStr = server.serverType match
+        case ServerType.Side    => s"+${server.rewardCpu} CPU, +${server.rewardRam} RAM"
+        case ServerType.Bank    => s"+${server.rewardCpu} CPU, +${server.rewardRam} Code"
+        case ServerType.GKS     => "🏁 Endziel"
+        case _                  => s"+${server.rewardCpu} CPU, +${server.rewardRam} RAM"
+      println(f"[$index%2d] ${server.name}%-20s | $contShort | Pos: (${pos._1}%2d, ${pos._2}%2d) | Typ: ${server.serverType}%-9s | Schwierigkeit: ${server.difficulty}%2d%% | $rewardStr")
+    }
 
-//   def show(players: List[Player], map: WorldMap, servers: List[Server]): Unit =
-//     println("Willkommen zu Codebreaker!")
-//     println("== Codebreaker: Weltkarte ==")
-//     println(displayMap(map, players, servers))
-//     println()
-//     players.zipWithIndex.foreach { case (p, i) =>
-//       println(s"Spieler $i: ${p.name} @ (${p.tile.x}, ${p.tile.y}) [${p.tile.continent.short}]")
-//       println(s"CPU: ${p.cpu} | RAM: ${p.ram} | Code: ${p.code}")
-//       println(s"Level: ${p.level} | XP: ${p.xp} | Security: ${p.cybersecurity}%")
-//       println("-" * 30)
-//     }
-//     println("== Serverliste ==")
-//     printServerList(servers, map)
-
-//   def printServerList(servers: List[Server], map: WorldMap): Unit =
-//     servers.zipWithIndex.foreach { case (server, index) =>
-//       val tile = server.tile
-//       val contShort = tile.continent.short
-//       val pos = (tile.x, tile.y)
-
-//       val rewardStr = server.serverType match
-//         case ServerType.Side    => s"+${server.rewardCpu} CPU, +${server.rewardRam} RAM"
-//         case ServerType.Bank    => s"+${server.rewardCpu} CPU, +${server.rewardRam} Code"
-//         case ServerType.GKS     => "🏁 Endziel"
-//         case _                  => s"+${server.rewardCpu} CPU, +${server.rewardRam} RAM"
-
-//       println(f"[$index%2d] ${server.name}%-20s | $contShort | Pos: (${pos._1}%2d, ${pos._2}%2d) | Typ: ${server.serverType}%-9s | Schwierigkeit: ${server.difficulty}%2d%% | $rewardStr")
-//     }
-
+  override def update: Unit =
+    println("Spielzustand hat sich geändert.")
+    show()
